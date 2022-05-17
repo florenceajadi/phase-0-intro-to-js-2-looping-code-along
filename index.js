376979ff7e95a8fd3928b67021763019411b5566
florence ajadi <florence.ajadi@yahoo.com>
2022-05-17 17:20:23 -0500

    commit tree 99df9b2c8b7d228c50a063e69921c9b6dda70dd6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+ let names = ["Guadalupe", "Ollie", "Aki"];
+ let callCount = 10;
+ 
+
+ function writeCards(names) {
+     const newArray = [];
+  for(let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+
+  }
+   return newArray
+}
+
+// function countDown(callCount) {
+    /// for(let callCount = 4; callCount >= 4; i++) {
+      ///  console.log()
+      ///  return callCount//
+// }} 
+// } 
+
+function countDown(number) {
+    // for(let i = 0; i < number; i++) {
+    //     console.log()
+    // }
+    while(number >= 0 ) {
+        console.log(number)
+    number--
+    // number -= 1
+    }
+    // console.log(number)
+}
